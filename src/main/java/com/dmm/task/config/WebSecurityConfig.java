af37dd39d3cfba5550c579cd6e70854fa7d214b6
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-08-17 17:11:29 +0900

    修正

--- src/main/java/com/dmm/task/config/WebSecurityConfig.java
+++ src/main/java/com/dmm/task/config/WebSecurityConfig.java
@@ -3,8 +3,8 @@ package com.dmm.task.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,8 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.dmm.task.service.AccountUserDetailsService;
 
-@Configuration // 設定を行うクラスであることを指定
-@EnableWebSecurity // Spring Securityを利用することを指定
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AccountUserDetailsService userDetailsService;
@@ -33,8 +34,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 認可の設定
-		http.authorizeRequests().antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
-			.anyRequest().authenticated(); // loginForm以外は、認証を求める
+		http.exceptionHandling().accessDeniedPage("/accessDeniedPage").and().authorizeRequests()
+				.antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
+				.anyRequest().authenticated(); // loginForm以外は、認証を求める
 
 		// ログイン設定
 		http.formLogin() // フォーム認証の有効化
@@ -42,18 +44,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/authenticate") // フォーム認証処理のパス
 				.usernameParameter("userName") // ユーザ名のリクエストパラメータ名
 				.passwordParameter("password") // パスワードのリクエストパラメータ名
-				.defaultSuccessUrl("/main") // 認証成功時に遷移するデフォルトのパス
+				.defaultSuccessUrl("/posts") // ※ ここを変更
 				.failureUrl("/loginForm?error=true"); // 認証失敗時に遷移するパス
 
 		// ログアウト設定
 		http.logout().logoutSuccessUrl("/loginForm") // ログアウト成功時に遷移するパス
 				.permitAll(); // 全ユーザに対して許可
-	}
-	
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		// 画像、JavaScript、cssは認可の対象外とする
-		web.debug(false).ignoring().antMatchers("/images/**", "/js/**", "/css/**");
 	}
 
 }
