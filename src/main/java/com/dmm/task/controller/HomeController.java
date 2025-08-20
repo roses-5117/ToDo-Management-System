232fba4c99a9ab3a649f276f125f3a9ced38f565
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-08-20 16:56:19 +0900

    mainメソッド内のタスク関連のコードをコメントアウト

--- src/main/java/com/dmm/task/controller/HomeController.java
+++ src/main/java/com/dmm/task/controller/HomeController.java
@@ -3,11 +3,11 @@ package com.dmm.task.controller;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
+// import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+// import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,18 +16,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.dmm.task.data.entity.Users;
-import com.dmm.task.data.repository.TaskRepository;
-import com.dmm.task.data.repository.UsersRepository;
+// import com.dmm.task.data.entity.Tasks;
+// import com.dmm.task.data.entity.Users;
+// import com.dmm.task.data.repository.TaskRepository;
+// import com.dmm.task.data.repository.UsersRepository;
 
 @Controller
 public class HomeController {
 
-    @Autowired
-    private TaskRepository taskRepository;
+    // @Autowired
+    // private TaskRepository taskRepository;
 
-    @Autowired
-    private UsersRepository usersRepository;
+    // @Autowired
+    // private UsersRepository usersRepository;
 
     @GetMapping("/main")
     public String main(@AuthenticationPrincipal UserDetails userDetails,
@@ -38,9 +39,8 @@ public class HomeController {
             date = LocalDate.now();
         }
 
-        Users loginUser = usersRepository.findById(userDetails.getUsername()).orElseThrow();
-
-        String role = loginUser.getRoleName();
+        // Users loginUser = usersRepository.findById(userDetails.getUsername()).orElseThrow();
+        // String role = loginUser.getRoleName();
 
         LocalDate prevMonth = date.minusMonths(1);
         LocalDate nextMonth = date.plusMonths(1);
@@ -49,7 +49,7 @@ public class HomeController {
         DayOfWeek startDayOfWeek = firstDayOfMonth.getDayOfWeek();
         int startDayValue = startDayOfWeek.getValue();
         if (startDayValue == 7) {
-             startDayValue = 0;
+            startDayValue = 0;
         }
 
         LocalDate startDay = firstDayOfMonth.minusDays(startDayValue);
@@ -66,20 +66,21 @@ public class HomeController {
             calendarMatrix.add(week);
         }
 
-        LocalDate endDay = currentDay.minusDays(1);
-        List<com.dmm.task.data.entity.Tasks> taskList;
+        // タスク関連のコードをコメントアウト
+        // LocalDate endDay = currentDay.minusDays(1);
+        // List<com.dmm.task.data.entity.Tasks> taskList;
 
-        if ("admin".equals(role)) {
-            taskList = taskRepository.findByDateBetween(startDay, endDay);
-        } else {
-            taskList = taskRepository.findByDateBetweenAndUser(startDay, endDay, loginUser);
-        }
+        // if ("admin".equals(role)) {
+        //     taskList = taskRepository.findByDateBetween(startDay, endDay);
+        // } else {
+        //     taskList = taskRepository.findByDateBetweenAndUser(startDay, endDay, loginUser);
+        // }
 
-        Map<LocalDate, List<com.dmm.task.data.entity.Tasks>> tasksMap = new HashMap<>();
-        for (com.dmm.task.data.entity.Tasks task : taskList) {
-            LocalDate taskDate = task.getDate();
-            tasksMap.computeIfAbsent(taskDate, k -> new ArrayList<>()).add(task);
-        }
+        // Map<LocalDate, List<com.dmm.task.data.entity.Tasks>> tasksMap = new HashMap<>();
+        // for (com.dmm.task.data.entity.Tasks task : taskList) {
+        //     LocalDate taskDate = task.getDate();
+        //     tasksMap.computeIfAbsent(taskDate, k -> new ArrayList<>()).add(task);
+        // }
 
 
         model.addAttribute("date", date);
@@ -87,7 +88,7 @@ public class HomeController {
         model.addAttribute("prev", prevMonth);
         model.addAttribute("next", nextMonth);
         model.addAttribute("matrix", calendarMatrix);
-        model.addAttribute("tasks", tasksMap);
+        // model.addAttribute("tasks", tasksMap);
 
         return "main";
     }
