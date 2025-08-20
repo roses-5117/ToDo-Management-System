8c27a968c780ac97c1be81bcc163b21855b18911
NoeShinohara <163381729+roses-5117@users.noreply.github.com>
2025-08-20 18:07:35 +0900

    Tasksに変更

--- src/main/java/com/dmm/task/data/repository/TaskRepository.java
+++ src/main/java/com/dmm/task/data/repository/TaskRepository.java
@@ -11,9 +11,7 @@ import com.dmm.task.data.entity.Tasks;
 import com.dmm.task.data.entity.Users;
 
 public interface TaskRepository extends JpaRepository<Tasks, Long> {
-
-    @Query("SELECT t FROM Task t WHERE t.date BETWEEN :start AND :end")
-    List<Tasks> findByDateBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
-
-    List<Tasks> findByDateBetweenAndUser(LocalDate start, LocalDate end, Users user);
+	@Query("SELECT t FROM Tasks t WHERE t.date BETWEEN :start AND :end")
+	List<Tasks> findByDateBetween(@Param("start") LocalDate start, @Param("end") LocalDate end);
+	List<Tasks> findByDateBetweenAndUser(LocalDate start, LocalDate end, Users user);
 }
